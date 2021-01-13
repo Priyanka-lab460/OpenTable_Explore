6154337a56ce85902d7129abec69d4ddd8785bec
Priya <priyankakadukar3@gmail.com>
2021-01-13 11:04:14 +0530

    2

--- Explore/src/test/java/cucumberTest/san_testrunner.java
+++ Explore/src/test/java/cucumberTest/san_testrunner.java
@@ -6,6 +6,7 @@ import cucumber.api.CucumberOptions;
 features= "features/san.feature"     //Name of feature file
 ,glue= {"stepDefinations"}                //Name of package
 //,tags= {"@tag"}
+,plugin = {"html:cucumber_reports","junit:cucumber_reports/cucumber.xml" }
 )
 public class san_testrunner extends cucumber.api.testng.AbstractTestNGCucumberTests{
 
